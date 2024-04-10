dc5bb70c2c7b3f0f333b60ced56d2843e8372f59
IrisRoh <iris.rohrmoser@gmx.at>
2024-04-10 08:53:01 +0200

    added test typing into input fields

---
+++ src/test/java/uitest/m4/TypingTest.java
@@ -0,0 +1,34 @@
+package uitest.m4;
+
+import helper.DemoHelper;
+import helper.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import static helper.Pages.*;
+
+public class TypingTest {
+
+    @Test
+    public void typingTest() {
+        WebDriver driver = DriverFactory.newDriver();
+        driver.get(HOME);
+
+        WebElement firstName = driver.findElement(By.id("firstName"));
+        WebElement lastName = driver.findElement(By.id("lastName"));
+        WebElement email = driver.findElement(By.id("email"));
+        WebElement birthday = driver.findElement(By.id("dob"));
+
+        firstName.sendKeys("Amanda");
+        lastName.sendKeys("Walker");
+        email.sendKeys("am.walker@email.com");
+        DemoHelper.pause();
+        email.clear();
+        email.sendKeys("am.walker@email.com");
+        birthday.sendKeys("27/03/2022");
+
+        DemoHelper.pause();
+        driver.quit();
+    }
+}
