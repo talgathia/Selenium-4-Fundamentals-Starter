80a09bf38347404d47539561f792fc7058f7c61a
IrisRoh <iris.rohrmoser@gmx.at>
2024-04-10 10:12:31 +0200

    ByLinkTest - switching sites by using linkText and partialLinkText

--- src/test/java/uitest/m4/ByLinkTest.java
+++ src/test/java/uitest/m4/ByLinkTest.java
@@ -1,4 +1,27 @@
 package uitest.m4;
 
+import helper.DemoHelper;
+import helper.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import static helper.Pages.*;
+
 public class ByLinkTest {
+
+    @Test
+    public void byLinkText() {
+        WebDriver driver = DriverFactory.newDriver();
+        driver.get(HOME);
+
+        driver.findElement(By.linkText("Savings")).click();
+        DemoHelper.pause();
+
+        driver.findElement(By.partialLinkText("Reg")).click();
+        DemoHelper.pause();
+
+        driver.quit();
+
+    }
 }
