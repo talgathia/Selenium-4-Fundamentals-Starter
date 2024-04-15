80b4ece685e213b3807457481aa188f8e29a1110
IrisRoh <iris.rohrmoser@gmx.at>
2024-04-15 08:36:21 +0200

    added test using Alert class to dismiss and accept alert

---
+++ src/test/java/uitest/m4/HandleDialogTest.java
@@ -0,0 +1,60 @@
+package uitest.m4;
+
+import helper.DemoHelper;
+import helper.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static helper.Pages.*;
+import org.openqa.selenium.Alert;
+
+public class HandleDialogTest {
+
+    @Test
+    void dismissAlertTest() {
+        WebDriver driver = DriverFactory.newDriver();
+        driver.get(HOME);
+
+        WebElement firstName = driver.findElement(By.id("firstName"));
+        WebElement lastName = driver.findElement(By.id("lastName"));
+
+        firstName.sendKeys("John");
+        lastName.sendKeys("Smith");
+
+        DemoHelper.pause();
+        driver.findElement(By.id("clear")).click();
+
+
+        Alert alert = driver.switchTo().alert();
+        alert.dismiss();
+        Assert.assertEquals(firstName.getAttribute("value"), "John");
+        Assert.assertEquals(lastName.getAttribute("value"), "Smith");
+
+        driver.quit();
+    }
+
+    @Test
+    void acceptAlertTest() {
+        WebDriver driver = DriverFactory.newDriver();
+        driver.get(HOME);
+
+        WebElement firstName = driver.findElement(By.id("firstName"));
+        WebElement lastName = driver.findElement(By.id("lastName"));
+
+        firstName.sendKeys("John");
+        lastName.sendKeys("Smith");
+
+        DemoHelper.pause();
+        driver.findElement(By.id("clear")).click();
+
+
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+        Assert.assertEquals(firstName.getAttribute("value"), "");
+        Assert.assertEquals(lastName.getAttribute("value"), "");
+
+        driver.quit();
+    }
+}
