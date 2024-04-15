c580c2c639e9a6de86bbe797a29d860ae1ceecf4
IrisRoh <iris.rohrmoser@gmx.at>
2024-04-15 08:23:53 +0200

    added test using relativeLocator class to get elements

---
+++ src/test/java/uitest/m4/RelativeLocatorsTest.java
@@ -0,0 +1,37 @@
+package uitest.m4;
+
+import helper.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import static helper.Pages.*;
+
+public class RelativeLocatorsTest {
+
+    WebDriver driver;
+
+    @Test
+    public void relativeLocator() {
+        driver = DriverFactory.newDriver();
+        driver.get(HOME);
+
+        WebElement email = driver.findElement(By.id("email"));
+        RelativeLocator.RelativeBy input = RelativeLocator.with(By.tagName("input"));
+        // NO WebElement but RelativeLocators
+
+        WebElement datePicker = driver.findElement(input.toRightOf(email));
+        System.out.println(datePicker.getAttribute("type")); // date
+
+        WebElement firstName = driver.findElement(input.above(email));
+        System.out.println(firstName.getAttribute("id")); // firstName
+
+    }
+
+    @AfterMethod
+    void cleanup() {
+        driver.quit();
+    }
+}
