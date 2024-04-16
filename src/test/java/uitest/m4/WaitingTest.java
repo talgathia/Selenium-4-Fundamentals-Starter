ade58366ba2d6572fa635ea3af2b346cf258b4b1
IrisRoh <iris.rohrmoser@gmx.at>
2024-04-16 12:14:37 +0200

    WaitingTest - added test that uses explicit wait

--- src/test/java/uitest/m4/WaitingTest.java
+++ src/test/java/uitest/m4/WaitingTest.java
@@ -2,13 +2,18 @@ package uitest.m4;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 import static helper.Pages.*;
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 
 public class WaitingTest {
 
@@ -30,5 +35,22 @@ public class WaitingTest {
         // logically, it's not clickable. But using actions like clicking and typing forces Selenium to wait for the element.
 
         driver.quit();
+    }
+
+
+    @Test
+    public void explicitWaitTest() {
+        driver = new ChromeDriver();
+        driver.get(LOANS);
+        driver.findElement(By.id("borrow")).sendKeys("500");
+
+        WebElement result = waitUntilClickable(driver, By.id("result"));
+        Assert.assertTrue(result.isDisplayed());
+
+        driver.quit();
+    }
+
+    public static WebElement waitUntilClickable(WebDriver driver, By locator) {
+        return new WebDriverWait(driver, Duration.ofSeconds(6)).until(elementToBeClickable(locator));
     }
 }
