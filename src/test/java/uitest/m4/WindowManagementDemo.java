73a1e5045b232e73bcfe44f0e9b0740f205086a7
IrisRoh <iris.rohrmoser@gmx.at>
2024-04-16 13:30:39 +0200

    WindowManagementDemo - finished test that switches between tabs

--- src/test/java/uitest/m4/WindowManagementDemo.java
+++ src/test/java/uitest/m4/WindowManagementDemo.java
@@ -1,16 +1,36 @@
 package uitest.m4;
 
-import org.openqa.selenium.WebDriver;
+import helper.DemoHelper;
+import helper.DriverFactory;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.*;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 import static helper.Pages.*;
 
 public class WindowManagementDemo {
 
     @Test
     public void tabsAndWindowsDemo() {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(); // window
         driver.get(HOME);
+
+        String firstTab = driver.getWindowHandle();
+
+        DemoHelper.pause();
+
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get(SAVINGS);
+        Assert.assertEquals(driver.getWindowHandles().size(), 2);
+
+        driver.switchTo().window(firstTab);
+        DemoHelper.pause();
+        driver.close();
+        Assert.assertEquals(driver.getWindowHandles().size(), 1);
 
         driver.quit();
     }
