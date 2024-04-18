1e3518939a9fd64f5f13c11a8660a6f8770f9ad4
IrisRoh <iris.rohrmoser@gmx.at>
2024-04-18 10:26:44 +0200

    added test that blocks javascript

--- src/test/java/uitest/m8/InterceptRequestDemo.java
+++ src/test/java/uitest/m8/InterceptRequestDemo.java
@@ -1,28 +1,54 @@
 package uitest.m8;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.v85.network.model.Request;
 import org.openqa.selenium.devtools.v85.network.model.Response;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import org.openqa.selenium.devtools.v85.network.Network;
-
-
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
-import static helper.Pages.*;
 
 
 public class InterceptRequestDemo {
 
     WebDriver driver;
     DevTools devTools;
+
+    @Test
+    public void manipulateTraffic() {
+
+        driver = new ChromeDriver();
+        devTools = getDevTool(driver);
+        devTools.send(Network.setBlockedURLs(List.of("*/footer.js"))); // blocks footer element
+        // if commented out, test will pass
+
+
+        driver.get("http://127.0.0.1:8000/index.html");
+
+
+        WebElement location = new WebDriverWait(driver, Duration.ofSeconds(2)).until(ExpectedConditions.visibilityOfElementLocated(By.id("location")));
+        Assert.assertTrue(location.getText().contains("You are visiting us from")); // should fail because we blocked it out
+
+    }
+
+    private static DevTools getDevTool(WebDriver driver) {
+        DevTools devTools = ((ChromeDriver) driver).getDevTools();
+        devTools.createSession();
+        devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+        return devTools;
+    }
+
 
     @Test
     public void howToGetDevToolsObject() {
